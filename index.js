56e81269580e8d67c557254f5f26ad8b3282f292
PRIYA SHARMA <ps3869253@gmail.com>
2025-04-01 21:36:51 +0530

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.get("/first",(req,res)=>{
-    res.send("First commit");
+app.post("/user/signup",(req,res)=>{
+    res.json({
+        message:"User created successfully"
+    })
 })
+app.post("/user/signin",(req,res)=>{
+    res.json({
+        message:"User logged in successfully"
+    })
+})
+
+app.post("/users/purchases", function(req,res){
+    res.json({
+       message: "purchase history"
+    })
+
+})
+
+app.post("/user/purchase" ,(req,res)=>{
+    res.json({
+        message: "course purchasing successfully"  
+    })
+})
+
+app.get("/courses",(req,res)=>{
+    res.json({
+
+    })
+})
+ 
 app.listen(3000)
