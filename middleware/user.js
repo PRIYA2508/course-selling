d02ff2149f4ea6eca4e30e304d947684f4f148fb
ps3869253@gmail.com <ps3869253@gmail.com>
2025-04-27 23:58:23 +0530

    correct more mistakes

--- middleware/user.js
+++ middleware/user.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const {JWT_USER_PASSWORD} = require("../config");
 
-function userMiddleware(){
+function userMiddleware(req,res,next){
 const token = req.headers.token;
 const decoded = jwt.verify(token , JWT_USER_PASSWORD);
 
